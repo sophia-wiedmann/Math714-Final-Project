b4d2b1fedfcd3946cfd9762790cee571d142abeb
eschondorf <37815736+eschondorf@users.noreply.github.com>
2023-12-14 15:39:16 -0600

    added function for diffusion with piecewise constant D

--- model.m
+++ model.m
@@ -1,7 +1,7 @@
 A = [1 2 3; 4 5 6; 7 8 9];
 D_test = cat(3, [1 2 3; 4 5 6; 7 8 9], [10 11 12; 13 14 15; 16 17 18], [19 20 21; 22 23 24; 25 26 27]);
 disp(D_test);
-[m, n, p] = size(D_test);
+[m, n_dim, p] = size(D_test);
 global k;
 global h;
 global n;
@@ -99,9 +99,9 @@ function c = diffusion_y(D, C, n, timestep)
             end
         end
     end
-    for i in 1:n
-        A_blk = diag(((1/mu) - 2*D_vec) *ones(1, n^2)) + diag(((-D_y((i-1)*n+1:i*n)/4)-D_vec((i-1)*n+1:i*n)/4))*ones(1, n^2 - n), n+1) + diag(((+D_y((i-1)*n+1:i*n)/4)-D_vec((i-1)*n+1:i*n)/4))*ones(1, n^2 - n), -1-n);
-        B_blk = diag(((1/mu) - 2*D_vec) *ones(1, n^2)) + diag(((D_y((i-1)*n+1:i*n)/4)+D_vec((i-1)*n+1:i*n)/4))*ones(1, n^2 - n), n+1) + diag(((-D_y((i-1)*n+1:i*n)/4)+D_vec((i-1)*n+1:i*n)/4))*ones(1, n^2 - n), -1-n);
+    for i = 1:n
+        A_blk = diag(((1/mu) - 2*D_vec) *ones(1, n^2)) + diag((((-D_y((i-1)*n+1:i*n)/4)-D_vec((i-1)*n+1:i*n)/4))*ones(1, n^2 - n), n+1) + diag((((D_y((i-1)*n+1:i*n)/4)-D_vec((i-1)*n+1:i*n)/4))*ones(1, n^2 - n), -1-n);
+        B_blk = diag(((1/mu) - 2*D_vec) *ones(1, n^2)) + diag((((D_y((i-1)*n+1:i*n)/4)+D_vec((i-1)*n+1:i*n)/4))*ones(1, n^2 - n), n+1) + diag((((-D_y((i-1)*n+1:i*n)/4)+D_vec((i-1)*n+1:i*n)/4))*ones(1, n^2 - n), -1-n);
 
         if i == 1
             A = A_blk;
@@ -135,8 +135,8 @@ end
 
 
 function c = advection_x(C, n, timestep)
-    A = diag((1/(v*nu)) *ones(1, n^3)) - diag(ones(1, n^3-1), 1) + diag(*ones(1, n^3-1), -1);
-    B = diag((1/(v*nu)) *ones(1, n^3)) + diag(ones(1, n^3-1), 1) - diag(*ones(1, n^3-1), -1);
+    A = diag((1/(v*nu)) *ones(1, n^3)) - diag(ones(1, n^3-1), 1) + diag(ones(1, n^3-1), -1);
+    B = diag((1/(v*nu)) *ones(1, n^3)) + diag(ones(1, n^3-1), 1) - diag(ones(1, n^3-1), -1);
     
     c = A \ (B * C);
 end
@@ -144,7 +144,7 @@ end
 
 function c = advection_y(C, n, timestep)
 
-    for i in 1:n
+    for i = 1:n
         A_blk = diag(1/(v*nu) *ones(1, n^2)) - diag(ones(1, n^2 - n), n+1) + diag(ones(1, n^2 - n), -1-n);
         B_blk = diag(1/(v*nu) *ones(1, n^2)) + diag(ones(1, n^2 - n), n+1) - diag(ones(1, n^2 - n), -1-n);
 
@@ -163,8 +163,8 @@ end
 
 
 function c = advection_z(C, n, timestep)
-    A = diag((1/(v*nu)) *ones(1, n^3)) - diag(ones(1, n^3-n^2), 1) + diag(*ones(1, n^3-n^2), -1);
-    B = diag((1/(v*nu)) *ones(1, n^3)) + diag(ones(1, n^3-n^2), 1) - diag(*ones(1, n^3-n^2), -1);
+    A = diag((1/(v*nu)) *ones(1, n^3)) - diag(ones(1, n^3-n^2), 1) + diag(ones(1, n^3-n^2), -1);
+    B = diag((1/(v*nu)) *ones(1, n^3)) + diag(ones(1, n^3-n^2), 1) - diag(ones(1, n^3-n^2), -1);
     
     c = A \ (B * C);
 end
@@ -173,11 +173,32 @@ function R = R(t)
     R = R_0 + v_c * t
 end
 
-
+%{
 function c = reaction(C, n, timestep, t)
     c = C + timestep*(s*delta((i,j,k) - R(t)) + rho*C(1-C))
     
 
 end
+%}
 
 
+function c = basic_3d_diffusion(D, C, n, timestep)
+    mu = timestep/(h^2)
+
+    for i = 1:n
+        A_blk = - diag(ones(1, n^2 - n), n+1) - diag(ones(1, n^2 - n), -1-n);
+        B_blk =  diag(ones(1, n^2 - n), n+1) + diag(ones(1, n^2 - n), -1-n);
+
+        if i == 1
+            A = A_blk;
+            B = B_blk;
+        else
+            A = blkdiag(A, A_blk);
+            B = blkdiag(B, B_blk);
+        end
+    end
+    A = A + diag((2/(mu * reshape(D, 1, [])) +6) *ones(1, n^3)) - diag(ones(1, n^3-1), 1) - diag(ones(1, n^3-1), -1) - diag(ones(1, n^3-n^2), 1) - diag(ones(1, n^3-n^2), -1); 
+    B = B + diag((2/(mu * reshape(D, 1, [])) -6) *ones(1, n^3)) + diag(ones(1, n^3-1), 1) + diag(ones(1, n^3-1), -1) + diag(ones(1, n^3-n^2), 1) + diag(ones(1, n^3-n^2), -1); 
+    
+    c = A \ (B * C);
+end
