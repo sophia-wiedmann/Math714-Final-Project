88c4d5868896d4e7603b32dd837009b20ea0fc6a
Sophia Wiedmann <sophia.wiedmann@gmail.com>
2023-12-15 11:49:39 -0600

    Function that computes diffusion coefficient at each gridpoint

---
+++ diffusionCoeff.m
@@ -0,0 +1,36 @@
+function D = diffusionCoeff(greyVol,whiteVol)
+% Computes diffusion coefficient matrix D (3D matrix)
+
+% Inputs: greyVol and whiteVol are 3D matrices describing what percentage of
+    % each cell is grey (white) matter in range [0,1]
+
+% Output: 3D matrix D where D(x,y,z) is diffusion coefficient at point
+    % (x,y,z) computed based on percentage of grey and white matter (0
+    % everywhere else)
+
+
+% Tissue-specific diffusion coefficients (need to update)
+D_grey = 1;
+D_white = 1;
+
+% Get dimensions of greyVol
+sz = size(greyVol);
+xdim = sz(1);
+ydim = sz(2);
+zdim = sz(3);
+
+% Initialize diffusion coefficient matrix
+D = zeros(xdim,ydim,zdim);
+
+% Compute diffusion coefficent for each cell
+for x = 1:xdim
+    for y = 1:ydim
+        for z = 1:zdim
+            greyVal = greyVol(x,y,z);
+            whiteVal = whiteVol(x,y,z);
+            D(x,y,z) = greyVal*D_grey + whiteVal*D_white;
+        end
+    end
+end
+
+end
