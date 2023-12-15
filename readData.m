6f650c5deb5df797163570c941dc7c74622e5771
Sophia Wiedmann <sophia.wiedmann@gmail.com>
2023-12-15 09:42:22 -0600

    Rescaled MRI data to [0,1] and removed plotting

--- readData.m
+++ readData.m
@@ -1,4 +1,9 @@
-%%% Script to read in MRI data and plot images
+%%% Reads in MRI data from simulated normal brain (BrainWeb)
+
+% Starting measurements in mm
+% zStart = -72;
+% yStart = -126;
+% xStart = -90;
 
 %% Read in anatomical structure data
 
@@ -21,7 +26,7 @@ filenames = [grey white skull skin connective CSF ...
 % Container for all the volume data
 volumeData = {};
 
-% Read out the data
+% Read in the data
 for i = 1:length(filenames)
     % Read to an array in volumeData
     filename = path + filenames(i);
@@ -31,6 +36,9 @@ for i = 1:length(filenames)
 
     % Reshape to x y z dimensions
     volumeData{i} = reshape(volumeData{i},181,217,181);
+
+    % Rescale MRI data to [0,1]
+    volumeData{i} = 1/255*volumeData{i};
 end
 
 % Volume data containers
@@ -44,105 +52,3 @@ fatVol = volumeData{7};
 glialVol = volumeData{8};
 muscleVol = volumeData{9};
 backgroundVol = volumeData{10};
-
-
-%% Get image data for a 2D slice
-
-% Starting measurements in mm
-zStart = -72;
-yStart = -126;
-xStart = -90;
-
-% Slice measurement in mm (change this to get a different slice)
-% Should get same image as in BrainWeb with given z value
-z = 28;
-
-% Get matching index
-zval = z-zStart;
-
-% Get MRI image data for 2D slice at z = zval
-greyData = greyVol(:,:,zval);
-whiteData = whiteVol(:,:,zval);
-skullData = skullVol(:,:,zval);
-skinData = skinVol(:,:,zval);
-connectiveData = connectiveVol(:,:,zval);
-CSFData = CSFVol(:,:,zval);
-fatData = fatVol(:,:,zval);
-glialData = glialVol(:,:,zval);
-muscleData = muscleVol(:,:,zval);
-backgroundData = backgroundVol(:,:,zval);
-
-
-%% Plot 2D slices
-
-% Flip x and y for plotting with surf
-greyData = greyData';
-whiteData = whiteData';
-skullData = skullData';
-skinData = skinData';
-connectiveData = connectiveData';
-CSFData = CSFData';
-fatData = fatData';
-glialData = glialData';
-muscleData = muscleData';
-backgroundData = backgroundData';
-
-figure;
-surf(greyData);
-colorbar;
-title("Grey Matter");
-axis equal
-
-figure;
-surf(whiteData);
-colorbar;
-title("White Matter");
-axis equal
-
-figure;
-surf(skullData);
-colorbar;
-title("Skull");
-axis equal
-
-figure;
-surf(skinData);
-colorbar;
-title("Skin");
-axis equal
-
-figure;
-surf(connectiveData);
-colorbar;
-title("Connective Tissue"); % What is this?
-axis equal
-
-figure;
-surf(CSFData);
-colorbar;
-title("Cerebrospinal Fluid (CSF)");
-axis equal
-
-figure;
-surf(fatData);
-colorbar;
-title("Fat");
-axis equal
-
-figure;
-surf(glialData);
-colorbar;
-title("Glial Matter");
-axis equal
-
-figure;
-surf(muscleData);
-colorbar;
-title("Muscle and Skin");
-axis equal
-
-figure;
-surf(backgroundData);
-colorbar;
-title("Background");
-axis equal
