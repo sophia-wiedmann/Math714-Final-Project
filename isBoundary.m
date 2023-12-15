23477653b366c8617a535b949dd6b159b0ad0f2c
Sophia Wiedmann <sophia.wiedmann@gmail.com>
2023-12-15 17:49:45 -0600

    Fixed error in if statement conditions

--- isBoundary.m
+++ isBoundary.m
@@ -1,9 +1,18 @@
-function test = isBoundary(point,skullVol,backgroundVol)
+function test = isBoundary(point)
 % Checks if a gridpoint is in the boundary (skull or background) -- should
 % include ventricles?
 % Inputs: point = [x, y, z] is gridpoint being checked, skullVol and
     % backgroundVol contain locations of boundary as determined by readData
 % Output: test = 1 if it's a boundary gridpoint (0 if not)
+
+% Dimensions of MRI data
+global xdim
+global ydim
+global zdim
+
+% MRI data
+global skullVol
+global backgroundVol
 
 % Set threshold levels for determining boundaries
 skull_threshold = 0.1; % need to decide what these values should be
@@ -14,16 +23,10 @@ x = point(1);
 y = point(2);
 z = point(3);
 
-% Dimensions of MRI data
-sz = size(skullVol);
-xdim = sz(1);
-ydim = sz(2);
-zdim = sz(3);
-
 % Check if it's in the boundary
-if (x > xdim) || (y > ydim) || (z > zdim)
+if (x > xdim | y > ydim | z > zdim)
     test = 1;
-elseif (skullVol(x,y,z) >= skull_threshold) || (backgroundVol(x,y,z) >= background_threshold)
+elseif (skullVol(x,y,z) >= skull_threshold | backgroundVol(x,y,z) >= background_threshold)
     test = 1;
 else
     test = 0;
