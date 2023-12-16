2788ce4fa2c16b5525228d0dbe30cf88bf48af1d
Sophia Wiedmann <sophia.wiedmann@gmail.com>
2023-12-16 09:30:09 -0600

    Removed commented out code

--- solver.m
+++ solver.m
@@ -2,9 +2,9 @@ function C_np1 = solver(C,F)
 % Solve differential equation for one time step
 % Inputs: 
     % C is 2D matrix of concentrations at points (x,y) at time t = n and
-    % fixed z value
+    % fixed z value (in mm)
     % F is matrix resulting from discretization of spatial components of
-    % differential equation
+    % differential equation (see buildF.m)
 % Output: C_nplus1 is concentration at time t = n+1
 
 global h
@@ -13,11 +13,8 @@ global xdim
 global ydim
 % global zdim
 
-% Turn C into vector
 % numPoints = xdim*ydim*zdim;
-% C = reshape(C,numPoints,1,1);
 numPoints = xdim*ydim;
-C = reshape(C,numPoints,1);
 
 % Construct matrices
 I = speye(numPoints);
@@ -27,10 +24,6 @@ BC = B*C;
 
 % Solve A*C_nplus1 = BC
 C_np1 = A\BC;
-
-% Reshape to (x,y,z) 3D matrix
-% C_nplus1 = reshape(C_nplus1,xdim,ydim,zdim);
-C_np1 = reshape(C_np1,xdim,ydim);
 
 end
 
