d595159071d7ba43a1418bc123a7382e5bd50e57
Sophia Wiedmann <sophia.wiedmann@gmail.com>
2023-12-19 23:51:08 -0600

    Include CSF as anatomical boundary

--- isBoundary.m
+++ isBoundary.m
@@ -12,10 +12,12 @@ global zdim
 % MRI data
 global skullVol
 global backgroundVol
+global CSFVol
 
 % Set threshold levels for determining boundaries
 skull_threshold = 0.1;
 background_threshold = 0.1;
+CSF_threshold = 0.1;
 
 % Coordinates of gridpoint
 x = point(1);
@@ -25,7 +27,11 @@ z = point(3);
 % Check if it's in the boundary
 if (x > xdim | y > ydim | z > zdim)
     test = 1;
-elseif (skullVol(x,y,z) >= skull_threshold | backgroundVol(x,y,z) >= background_threshold)
+elseif skullVol(x,y,z) >= skull_threshold
+    test = 1;
+elseif backgroundVol(x,y,z) >= background_threshold
+    test = 1;
+elseif CSFVol(x,y,z) >= CSF_threshold
     test = 1;
 else
     test = 0;
