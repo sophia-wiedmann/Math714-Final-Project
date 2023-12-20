fbb922d7ac843fea302049a44c34e600c1e8451b
Sophia Wiedmann <sophia.wiedmann@gmail.com>
2023-12-19 22:59:50 -0600

    Updated for 2D simulation

--- diffusionCoeff.m
+++ diffusionCoeff.m
@@ -15,30 +15,22 @@ global whiteVol
 % MRI data dimensions
 global xdim
 global ydim
-% global zdim
 
 zStart = -72;
 zval = z-zStart;
 
-% Tissue-specific diffusion coefficients from Table 11.6 in textbook
-    % Units are cm^2/day
-    % Tumor grading is high (HH), intermediate (HL), intermediate (LH),
-    % and low (LL)
 Dw = 5*Dg;
 
 % Initialize diffusion coefficient matrix as square matrix using max
 % dimension
-% D = zeros(xdim, ydim, zdim);
 D = zeros(xdim, ydim);
 
 % Compute diffusion coefficent for each cell
 for x = 1:xdim
     for y = 1:ydim
-         % for z = 1:zdim
-            greyVal = greyVol(x,y,zval);
-            whiteVal = whiteVol(x,y,zval);
-            D(x,y) = greyVal*Dg + whiteVal*Dw;
-        % end
+        greyVal = greyVol(x,y,zval);
+        whiteVal = whiteVol(x,y,zval);
+        D(x,y) = greyVal*Dg + whiteVal*Dw;
     end
 end
 
