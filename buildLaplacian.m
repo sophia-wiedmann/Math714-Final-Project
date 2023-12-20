4fc3dfc1214bbf87cf471a50a577907c142539bb
Sophia Wiedmann <sophia.wiedmann@gmail.com>
2023-12-19 22:59:33 -0600

    Updated for 2D simulation

--- buildLaplacian.m
+++ buildLaplacian.m
@@ -5,81 +5,56 @@ function D2 = buildLaplacian(z)
 % Constants
 global xdim
 global ydim
-% global zdim
 global h
 
 zStart = -72;
 zval = z-zStart;
 
-%numPoints = xdim*ydim*zdim;
-%sz = [xdim, ydim, zdim];
 numPoints = xdim*ydim;
 sz = [xdim, ydim];
 
 % Initialize D2 as sparse matrix
-%numNonzero = 7*numPoints;
 numNonzero = 5*numPoints;
 D2 = spalloc(numPoints,numPoints,numNonzero);
 
 for ijk = 1:numPoints
     % Get (x,y,z) coordinates for point
-    %[i, j, k] = ind2sub(sz, ijk);
     [i, j] = ind2sub(sz, ijk);
 
     % Update diagonal entry of D2
-    %D2(ijk,ijk) = -6*hfac;
     D2(ijk,ijk) = -4;
 
     % Indices of neighbors
     if i+1 <= xdim
-        %i1jk = sub2ind(sz, i+1, j, k);
         i1jk = sub2ind(sz, i+1, j);
     else
         i1jk = 0;
     end
 
     if i-1 >= 1
-        %im1jk = sub2ind(sz, i-1, j, k);
         im1jk = sub2ind(sz, i-1, j);
     else
         im1jk = 0;
     end
 
     if j+1 <= ydim
-        %ij1k = sub2ind(sz, i, j+1, k);
         ij1k = sub2ind(sz, i, j+1);
     else
         ij1k = 0;
     end
 
     if j-1 >= 1
-        %ijm1k = sub2ind(sz, i, j-1, k);
         ijm1k = sub2ind(sz, i, j-1);
     else
         ijm1k = 0;
     end
 
-    % if k+1 <= zdim
-    %     ijk1 = sub2ind(sz, i, j, k+1);
-    % else
-    %     ijk1 = 0;
-    % end
-    % 
-    % if k-1 >= 1
-    %     ijkm1 = sub2ind(sz, i, j, k-1);
-    % else
-    %     ijkm1 = 0;
-    % end
-
-    %neighbors = [i1jk im1jk ij1k ijm1k ijk1 ijkm1];
     neighbors = [i1jk im1jk ij1k ijm1k];
 
     % Check if orthogonal neighbors are in the boundary and update D2
     for index = 1:length(neighbors)
         neighbor = neighbors(index);
         if neighbor ~= 0 % Neighbor is in grid
-            % [nbr_x, nbr_y, nbr_z] = ind2sub(sz,neighbor);
-            % point = [nbr_x, nbr_y, nbr_z];
             [nbr_x, nbr_y] = ind2sub(sz,neighbor);
             point = [nbr_x, nbr_y, zval];
         else point = [1, 1, 1]; % just assign a random point
